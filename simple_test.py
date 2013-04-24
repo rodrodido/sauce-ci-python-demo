1206554c51b85224d21f873dd2f20a231936d614
Ross Rowe <piaross@gmail.com>
2013-04-25 08:28:17 +1000

    Handle SELENIUM_VERSION not being specified

--- simple_test.py
+++ simple_test.py
@@ -13,8 +13,7 @@ class testSauceWrappers(unittest.TestCase):
     def setUp(self):
         desired_capabilities = {}
         desired_capabilities['browserName'] = os.environ['SELENIUM_BROWSER']
-        if (os.environ['SELENIUM_VERSION'])
-            desired_capabilities['version'] = os.environ.get['SELENIUM_VERSION']
+        desired_capabilities['version'] = os.getenv('SELENIUM_VERSION', '')
         desired_capabilities['platform'] = os.environ['SELENIUM_PLATFORM']
         command_executor = "http://%s:%s@%s:%s/wd/hub" % (os.environ['SAUCE_USER_NAME'], os.environ['SAUCE_API_KEY'], os.environ['SELENIUM_HOST'], os.environ['SELENIUM_PORT'])
         self.driver = webdriver.Remote(desired_capabilities=desired_capabilities, command_executor=command_executor)
